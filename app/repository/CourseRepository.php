a3b4d424e48491ae8020ba3f1b0926acf4fc55f1
Артем Полянский <mergenmasterf1@gmail.com>
2020-04-12 16:56:31 +0300

    немного красивее

--- app/repository/CourseRepository.php
+++ app/repository/CourseRepository.php
@@ -10,9 +10,14 @@ class CourseRepository
     /**
      * @param int $userId идентификатор студента
      * @return array массив параметров курсов
+     * @throws
      */
     public function getPayedByUserId(int $userId): array
     {
-        return \Yii::$app->db->createCommand('SELECT * FROM course where id in(SELECT course_id from course_group where id in (select course_group_id from payment where approved_by is not null and student_id = :id))')->bindParam(':id', $userId)->queryAll();
+        return \Yii::$app->db->createCommand(
+            'SELECT * FROM course where id in
+                    (SELECT course_id from course_group where id in 
+                    (select course_group_id from payment where approved_by is not null and student_id = :id))'
+        )->bindParam(':id', $userId)->queryAll();
     }
 }
