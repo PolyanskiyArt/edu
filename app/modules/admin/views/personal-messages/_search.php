d0624cd1955966c4f701b794c3639ad914d01d2c
papaha <papaha@bk.ru>
2020-05-09 19:07:35 +0700

    First commit

--- app/modules/admin/views/personal-messages/_search.php
+++ app/modules/admin/views/personal-messages/_search.php
@@ -18,11 +18,6 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'is_new')->checkbox(['onchange'=> '$("button[type=\'submit\'").click();'])->label(''); ?>
     <?= Html::submitButton('Фильтр', ['class' => 'btn btn-primary', 'style'=>'display:none']) ?>
 
-<!--    <div class="form-group">-->
-<!--        --><?//= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
-<!--        --><?//= Html::resetButton('Reset', ['class' => 'btn btn-default']) ?>
-<!--    </div>-->
-
     <?php ActiveForm::end(); ?>
 
 </div>
