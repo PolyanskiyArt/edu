0a9342e7da95d99f51f6efa74d65eafd9782f9e6
papaha <papaha@bk.ru>
2020-05-10 16:45:08 +0700

    Выполнено. https://education.planfix.ru/task/32

--- app/modules/admin/views/personal-messages/_create.php
+++ app/modules/admin/views/personal-messages/_create.php
@@ -11,21 +11,21 @@ use yii\helpers\ArrayHelper;
 <div class="message-create">
 
     <?php $form = ActiveForm::begin([
-        'layout'=>'horizontal',
+        'layout' => 'horizontal',
         'action' => ['create'],
         'method' => 'post',
     ]); ?>
 
-    <?= $form->field($model, 'text')->textarea(['rows' => 2, 'cols' => 5])->label('Многострочное текстовое поле');?>
+    <?= $form->field($model, 'text')->textarea(['rows' => 2, 'cols' => 5])->label('Текст сообщения'); ?>
     <?php
     $id = Yii::$app->user->id;
-    $users = User::find()->where('id != :id', [':id' => $id])->orderby(['username'=>SORT_ASC])->all();
+    $users = User::find()->where('id != :id', [':id' => $id])->orderby(['username' => SORT_ASC])->all();
     // формируем массив, с ключем равным полю 'id' и значением равным полю 'name'
     $items = ArrayHelper::map($users, 'id', 'username');
     $params = [
-    'prompt' => 'Кому письмо'
+        'prompt' => 'Кому письмо'
     ];
-    echo $form->field($model, 'to_user_id')->dropDownList($items,$params);
+    echo $form->field($model, 'to_user_id')->dropDownList($items, $params);
     ?>
 
     <?= Html::submitButton('Отправить', ['class' => 'btn btn-primary']) ?>
--- app/modules/admin/views/personal-messages/_search.php
+++ app/modules/admin/views/personal-messages/_search.php
@@ -8,13 +8,13 @@ use yii\bootstrap\ActiveForm;
 <div class="message-search">
 
     <?php $form = ActiveForm::begin([
-        'layout'=>'horizontal',
+        'layout' => 'horizontal',
         'action' => ['index'],
         'method' => 'get',
     ]); ?>
 
-    <?= $form->field($model, 'is_new')->checkbox(['onchange'=> '$("button[type=\'submit\'").click();'])->label('Только новые'); ?>
-    <?= Html::submitButton('Фильтр', ['class' => 'btn btn-primary', 'style'=>'display:none']) ?>
+    <?= $form->field($model, 'is_new')->checkbox(['onchange' => '$("button[type=\'submit\'").click();'])->label('Только новые'); ?>
+    <?= Html::submitButton('Фильтр', ['class' => 'btn btn-primary', 'style' => 'display:none']) ?>
 
     <?php ActiveForm::end(); ?>
 
